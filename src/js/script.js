282842e23984ee243efcef810f7b7d61aa135e38
web-kirarin <coruppe1114@yahoo.co.jp>
2024-04-20 19:09:12 +0900

    ハンバーガーメニュー完了！

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,12 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass('is-active')) {
+    $('.js-hamburger').removeClass("is-active");
+    $(".js-sp-nav").fadeOut(300);
+    } else {
+    $('.js-hamburger').addClass("is-active");
+    $(".js-sp-nav").fadeIn(300);
+    }
+    });
 });
