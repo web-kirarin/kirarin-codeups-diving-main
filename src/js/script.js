f41e54369de6b7e933b2679f6362f662fae2be6d
web-kirarin <coruppe1114@yahoo.co.jp>
2024-04-21 17:10:03 +0900

    ドロワーメニューのjsもバッチリ

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,7 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-  $(".js-hamburger").click(function () {
-    if ($(".js-hamburger").hasClass('is-active')) {
-    $('.js-hamburger').removeClass("is-active");
-    $(".js-sp-nav").fadeOut(300);
-    } else {
-    $('.js-hamburger').addClass("is-active");
-    $(".js-sp-nav").fadeIn(300);
-    }
-    });
+  $(".js-hamburger,.js-sp-nav").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-sp-nav").toggleClass("is-active");
+  });
 });
