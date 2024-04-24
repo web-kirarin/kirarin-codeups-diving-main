c4d0ccd256af374b2b75f209012db1d988b4a9e4
web-kirarin <coruppe1114@yahoo.co.jp>
2024-04-24 21:57:21 +0900

    fvの画像保存した

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,20 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-hamburger").toggleClass("is-active");
     $(".js-sp-nav").toggleClass("is-active");
   });
+  //スライダー
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 3000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3000,
+    },
+    on: {
+      resize: function () {
+        swiper.autoplay.start();
+      }
+    }
 });
+});
+
