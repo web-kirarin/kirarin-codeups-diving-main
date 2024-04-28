2337fc4b2ed535f39c5b706e8d56daa7538f08ae
web-kirarin <coruppe1114@yahoo.co.jp>
2024-04-28 15:02:52 +0900

    2つめのスライダーが動かない

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-sp-nav").toggleClass("is-active");
   });
   //スライダー
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper .fv-swiper", {
   loop: true,
   effect: "fade",
   speed: 3000,
@@ -18,6 +18,23 @@ const swiper = new Swiper(".swiper", {
       swiper.autoplay.start();
     }
   }
+});
+
+const swiper = new Swiper(".swiper .cp-swiper", {
+  loop: true, // ループ
+  speed: 1500, // 少しゆっくり(デフォルトは300)
+  slidesPerView: 1.5, // 一度に表示する枚数
+  spaceBetween: 30, // スライド間の距離
+  centeredSlides: true, // アクティブなスライドを中央にする
+  autoplay: { // 自動再生
+    delay: 1000, // 1秒後に次のスライド
+    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+  },
+  // 前後の矢印
+  navigation: {
+    nextEl: ".cp-swiper-button-next",
+    prevEl: ".cp-swiper-button-prev",
+  },
 });
 });
 
