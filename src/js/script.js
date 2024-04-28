1ab949ba124ff7328d6dbe648400f57b9ec01db1
web-kirarin <coruppe1114@yahoo.co.jp>
2024-04-28 16:29:00 +0900

    メインビューがなんか変

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-sp-nav").toggleClass("is-active");
   });
   //スライダー
-const swiper = new Swiper(".swiper .fv-swiper", {
+const swiper01 = new Swiper(".fv-swiper .swiper", {
   loop: true,
   effect: "fade",
   speed: 3000,
@@ -20,21 +20,19 @@ const swiper = new Swiper(".swiper .fv-swiper", {
   }
 });
 
-const swiper = new Swiper(".swiper .cp-swiper", {
-  loop: true, // ループ
-  speed: 1500, // 少しゆっくり(デフォルトは300)
-  slidesPerView: 1.5, // 一度に表示する枚数
-  spaceBetween: 30, // スライド間の距離
-  centeredSlides: true, // アクティブなスライドを中央にする
-  autoplay: { // 自動再生
-    delay: 1000, // 1秒後に次のスライド
-    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+//スライダー2個目
+const swiper02 = new Swiper(".cp-swiper .swiper", {
+  loop: true,
+  speed:1500,
+  slidesPerView:1.2,
+  autoplay: {
+    delay: 1500,
   },
-  // 前後の矢印
-  navigation: {
-    nextEl: ".cp-swiper-button-next",
-    prevEl: ".cp-swiper-button-prev",
-  },
+  breakpoints: {
+    768:{
+      slidesPerView:3.5,
+    }
+  }
 });
 });
 
