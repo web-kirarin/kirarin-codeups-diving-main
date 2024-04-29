8a2f07d25f2799cc255168258f9b5026dfe8790b
web-kirarin <coruppe1114@yahoo.co.jp>
2024-04-29 18:12:42 +0900

    キャンペーン完成！

--- src/js/script.js
+++ src/js/script.js
@@ -5,35 +5,42 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-sp-nav").toggleClass("is-active");
   });
   //スライダー
-const swiper01 = new Swiper(".fv-swiper .swiper", {
-  loop: true,
-  effect: "fade",
-  speed: 3000,
-  allowTouchMove: false,
-  autoplay: {
-    delay: 3000,
-  },
-  on: {
-    resize: function () {
-      swiper.autoplay.start();
+  const swiper01 = new Swiper(".fv-swiper .swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 3000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3000,
+    },
+    on: {
+      resize: function () {
+        swiper.autoplay.start();
+      }
     }
-  }
-});
+  });
 
-//スライダー2個目
-const swiper02 = new Swiper(".cp-swiper .swiper", {
-  loop: true,
-  speed:1500,
-  slidesPerView:1.2,
-  autoplay: {
-    delay: 1500,
-  },
-  breakpoints: {
-    769:{
-      slidesPerView:3.5,
-    }
-  }
-});
+  //スライダー2個目
+  const swiper02 = new Swiper(".cp-swiper .swiper", {
+    loop: true,
+    speed: 1500,
+    slidesPerView: 1.3,
+    spaceBetween: 24,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 3.5,
+        spaceBetween: 40,
+      }
+    },
+    navigation: {
+      nextEl: ".cp-swiper-button-next",
+      prevEl: ".cp-swiper-button-prev",
+    },
+  });
 });
 
 
