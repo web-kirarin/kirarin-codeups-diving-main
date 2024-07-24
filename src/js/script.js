49e60680a6dd331e1a399fee6974d97582dbfe2f
web-kirarin <coruppe1114@yahoo.co.jp>
2024-07-24 16:25:58 +0900

    ドロワーメニューをふわっと表示させた！

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
   $(".js-hamburger,.js-sp-nav").click(function () {
     $(".js-hamburger").toggleClass("is-active");
-    $(".js-sp-nav").toggleClass("is-active");
+    $(".js-sp-nav").toggleClass("fade");
   });
   //ローディングアニメーションとスライダー1個目
   $(window).on("load", function () {
